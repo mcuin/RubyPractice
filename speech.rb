2e79003a9a59e1fe9c1f37ee01938143d59a5027
Mykal Cuin <mykcuin@gmail.com>
2015-02-13 16:29:54 -0700

    _why's guide 2. Speech: Numbers, Strings, Symbols, Constants

--- speech.rb
+++ speech.rb
@@ -6,3 +6,16 @@ gratuity = 9.08
 
 total = orphan_fee + teddy_bear_fee + gratuity
 
+population = 12_000_000_000
+pi = 3.14
+avril_quote = "I'm a lot wiser.  Now I know what the business is like -- what you have to do and how to work it."
+
+print orphan_fee
+print avril_quote
+print population
+
+a = :symbol
+
+EmpreStateBuilding = "350 5th Avenue, NYC, NY"
+
+
