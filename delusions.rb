0d6e385aef7c36e267b2325394daa8270a95bc7a
Mykal Cuin <mykcuin@gmail.com>
2015-02-14 11:19:08 -0700

    _why's guide 3. Chaning Delusions: Flipping Script

---
+++ delusions.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+print "Type and be diabolical: "
+idea_backwards = gets.reverse
+
+print idea_backwards
+print "\n"
+
+idea_backwards = gets.upcase.reverse
+
+print idea_backwards
+print "\n"
