6c806ab3a64c6d1bd286fdbe65dfbcd24abe1841
Mykal Cuin <mykcuin@gmail.com>
2015-02-14 11:27:10 -0700

    _why's guide 3. Chaning Delusions: Repetitivness

--- delusions.rb
+++ delusions.rb
@@ -10,3 +10,14 @@ idea_backwards = gets.upcase.reverse
 
 print idea_backwards
 print "\n"
+
+code_words = {
+	'starmonkeys' => 'Phil and Pete, those prickly chancellors of the New Reich.',
+	'catapult' => 'chucky go-go', 
+	'firebomb' => 'Heated-Assisted Living',
+	'Nigeria' => "Ny and Jerry's Dry Cleaning (with Donuts)",
+	'Put the kabosh on' => 'Put the cable box on'
+}
+
+print code_words.[]('catapult')
+print "\n"
